cacb2dfcf509da9edf02978e10e6f177da455e5f
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-05-10 13:35:08 +0545

    Update urls.py

--- blogapiapp/urls.py
+++ blogapiapp/urls.py
@@ -4,5 +4,10 @@ from .import views
 urlpatterns = [
        path('', views.index),
        path('get-all-posts/',views.GetAllPosts),
+       path('create-new-post/', views.CreatePost),
+       path('delete-post/', views.DeletePost),
+       path('get-post/',views.GetPost),
+       path('update-post/', views.UpdatePost),
+
 
 ]
