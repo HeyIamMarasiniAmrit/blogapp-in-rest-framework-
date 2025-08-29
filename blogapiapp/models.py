6b1193d0b002c02a4261b2a2764fe11e8e566be0
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-08-29 21:31:06 +0545

    Update models.py

--- blogapiapp/models.py
+++ blogapiapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
     likes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
 
